7d4105c0d49ee1bb5a7a9db1ee2d2755e9725be7
pattywang726 <wangpeiwen726@outlook.com>
2020-06-30 22:02:12 -0400

    updated

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,127 @@
-package PACKAGE_NAME;
+public class ArrayDeque<Item> {
+    private Item[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
 
-public class ArrayDeque {
+    /** The starting sizArrayDequee of your array should be 8. start my array at 4*/
+    public ArrayDeque() {
+        items = (Item[]) new Object[8]; // special way for creating generic Array list;
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    private void resize(int capacity){
+        Item[] a = (Item[]) new Object[capacity];
+        if (nextLast == 0){
+            System.arraycopy(items, 0, a, 0, size);
+            items = a;
+            nextFirst = capacity - 1;
+            nextLast = size;
+        } else {
+            System.arraycopy(items, nextFirst + 1, a, nextFirst + 1 + capacity - size, size - (nextFirst + 1));
+            System.arraycopy(items, 0, a, 0, nextLast);
+            items = a;
+            nextFirst = nextFirst + capacity - size; // size is still 8, but list length doubled.
+        }
+    }
+
+    public void addLast(Item x){
+        if (size == items.length){
+            resize(size * 2);
+        }
+        items[nextLast % items.length] = x;
+        size = size + 1;
+        nextLast = (nextLast % items.length + 1)%items.length;
+    }
+
+    public void addFirst(Item x){
+        if (size == items.length){
+            resize(size * 2);
+        }
+
+        items[(nextFirst+items.length) % items.length] = x;
+        size = size + 1;
+        nextFirst = (nextFirst + items.length) % items.length - 1;
+    }
+
+    public boolean isEmpty(){
+        if (size==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private int Non0nextLast(int nextLast){
+        if (nextLast == 0){
+            return size;
+        }else{
+            return nextLast;
+        }
+    }
+
+    private int Be0nextFirst(int nextFirst){
+        if (nextFirst+1 == size){
+            return size;
+        }else{
+            return nextLast;
+        }
+    }
+
+    public void printDeque(){
+        for (int i=nextFirst+1; i<items.length ; i+=1){
+            System.out.print(items[i]);
+            System.out.print(' ');
+        }
+        if (size >= items.length-nextFirst){
+            for (int j=0; j<Non0nextLast(nextLast) ; j+=1){
+                System.out.print(items[j]);
+                System.out.print(' ');
+        }
+        }
+    }
+    /** Returns the item from the last of the list. */
+    private Item getLast() {
+        return items[((nextLast + items.length) - 1) % items.length];
+    }
+
+    /** Returns the item from the first of the list. */
+    private Item getFirst() {
+        return items[(nextFirst + 1) % items.length];
+    }
+
+    public Item removeLast(){
+        if (size / Double.valueOf(items.length) <= 0.50 && items.length > 16)
+            resize(size * 2);
+
+        Item x = getLast();
+        items[nextLast - 1] = null;
+        size = size - 1;
+        nextLast = Non0nextLast(nextLast) - 1;
+        return x;
+    }
+
+    public Item removeFirst(){
+        if (size / Double.valueOf(items.length) <= 0.25 && items.length > 16)
+            resize(size * 2);
+
+        Item y = getFirst();
+        items[nextFirst + 1] = null;
+        size = size - 1;
+        nextFirst = (nextFirst + 1) % items.length;
+        return y;
+    }
+    public Item get(int index) {
+        if (index < items.length - nextFirst - 1) {
+            return items[index + nextFirst + 1];
+        } else {
+            return items[index - (items.length - nextFirst - 1)];
+        }
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,146 @@
-package PACKAGE_NAME;
+public class LinkedListDeque<BleepBlorp> {
+    private class StuffNode {
+        public BleepBlorp item;
+        public StuffNode prev;
+        public StuffNode next;
 
-public class LinkedListDeque {
+        public StuffNode(StuffNode m, BleepBlorp i, StuffNode n) {
+            prev = m;
+            item = i;
+            next = n;
+//            System.out.println(size);
+        }
+    }
+
+    /* The first item (if it exists) is at sentinel.next. */
+    private StuffNode sentinel;
+    private int size;
+//    public BleepBlorp[] list_item;
+
+    /** Creates an empty SLList. */
+    public LinkedListDeque() {
+        sentinel = new StuffNode(null,null,null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+//    public LinkedListDeque(BleepBlorp x) {
+//        sentinel = new StuffNode(null,null,null);
+//        sentinel.next = new StuffNode(null,x,null);
+//        sentinel.next.next = sentinel;
+//
+//        sentinel.prev = sentinel.next;
+//        sentinel.next.prev = sentinel;
+//
+//        size = 1;
+//    }
+
+    /** Adds x to the front of the list. */
+    public void addFirst(BleepBlorp x) {
+        if (isEmpty()){
+            sentinel.next = new StuffNode(sentinel, x, sentinel);
+            sentinel.prev = sentinel.next;
+        }else {
+            sentinel.next = new StuffNode(sentinel, x, sentinel.next);
+            sentinel.next.next.prev = sentinel.next.next;
+        }
+        size = size + 1;
+    }
+
+    public void addLast(BleepBlorp x) {
+        if (isEmpty()){
+            sentinel.prev = new StuffNode(sentinel.prev, x, sentinel);
+            sentinel.next = sentinel.prev;
+        }else {
+            sentinel.prev = new StuffNode(sentinel.prev, x, sentinel);
+            sentinel.prev.prev.next = sentinel.prev;
+        }
+        size = size + 1;
+    }
+
+    // Returns true if deque is empty, false otherwise. //
+    public boolean isEmpty(){
+        if (size == 0){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        if (isEmpty()){
+            System.out.print(' ');
+        }
+
+        StuffNode p = sentinel.next;
+
+        for(int i=0; i<size; i+=1){
+            System.out.print(p.item);
+            System.out.print(' ');
+            p = p.next;
+            }
+    }
+
+    public BleepBlorp removeFirst(){
+        if (isEmpty()){
+            return null;
+        }
+        BleepBlorp First = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size = size - 1;
+
+        return First;
+    }
+
+    public BleepBlorp removeLast(){
+        if (isEmpty()){
+            return null;
+        }
+        BleepBlorp Last = sentinel.next.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+
+        size = size - 1;
+        return Last;
+    }
+
+    public BleepBlorp get(int index){
+        if (index >= size){
+            return null;
+        }
+
+        StuffNode p = sentinel.next;
+
+        for(int i=0; i<size; i+=1){
+            if (i == index){
+                break;
+            }
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    /**
+     * Same as get, but uses recursion
+     * First, need a private helper method
+     */
+    private BleepBlorp getRecursive(int index, StuffNode curr){
+        if (index == (size()-1)) {
+            return curr.item;
+        }
+        return getRecursive(index+1, curr.prev);
+    }
+
+    public BleepBlorp getRecursive(int index){
+        if (index >= size()){
+            return null;
+        }
+        return getRecursive(index, sentinel.prev);
+    }
 }
