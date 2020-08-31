4c97b72b1d928231af76d783796a9fb0472f2943
pattywang726 <wangpeiwen726@outlook.com>
2020-08-31 10:47:11 -0400

    completed MazeAstarpath

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +13,7 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private MinPQ<Integer> PQ;
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,12 +22,23 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        PQ = new MinPQ<Integer>(maze.V(), new sort());
+        PQ.insert(s);
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
+
+    class sort implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return h((int) o1) - h((int) o2);
+        }
+    }
+
 
     /** Finds vertex estimated to be closest to target. */
     private int findMinimumUnmarked() {
@@ -31,7 +48,24 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        s = PQ.delMin();
+        marked[s] = true;
+        announce();
+        while (s != t) {
+            for (int neighbor : maze.adj(s)) {
+                if (!marked[neighbor] && neighbor != edgeTo[s]) {
+                    PQ.insert(neighbor);
+                }
+            }
+            int v = PQ.delMin();
+            marked[v] = true;
+            edgeTo[v] = s;
+            distTo[v] = distTo[s] + 1;
+            announce();
+            s = v;
+        }
+        // Done
+
     }
 
     @Override
