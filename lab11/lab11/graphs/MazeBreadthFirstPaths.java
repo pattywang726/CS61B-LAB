e6fb7b6f4e2b49ef743e39976b082916201a420e
pattywang726 <wangpeiwen726@outlook.com>
2020-08-30 21:16:16 -0400

    completed lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,20 +13,53 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> queue;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
+        // refer to the constructor: MazeSolver(Maze m) in the superClass: MazeSolver.
         super(m);
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        queue = new Queue<Integer>();
+        queue.enqueue((Integer) s);
         // Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v) {
+        marked[v] = true;
+        announce();
+
+        while (!queue.isEmpty()) {
+            int w = (int) queue.dequeue();
+            if (w == t) {
+                targetFound = true;
+                return;
+            }
+            for (int neighbors : maze.adj(w)) {
+                if (!marked[neighbors]) {
+                    queue.enqueue((Integer) neighbors);
+                    distTo[neighbors] = distTo[w] + 1;
+                    marked[neighbors] = true;
+                    edgeTo[neighbors] = w;
+                    announce();
+                }
+            }
+        }
+        // DONE: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
     }
 
 
     @Override
     public void solve() {
+        bfs(s);
         // bfs();
     }
 }
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.nio.charset.IllegalCharsetNameException;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +14,56 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private WeightedQuickUnionUF WQU;
+    private Maze maze;
+    private Stack<Integer> stack;
+    private boolean cycle = false;
+    private int s;
+    private int [] dashEdgeTo;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(1,1);
+        dashEdgeTo = new int[maze.V()];
+        WQU = new WeightedQuickUnionUF(m.V());
+        stack = new Stack<Integer>();
+        stack.push(s);
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        checkCycle();
+        // DONE: Your code here!
     }
 
     // Helper methods go here
+    private void checkCycle() {
+        while (!cycle) {
+            s = stack.pop();
+            for (int next : maze.adj(s)) {
+                if (!marked[next]) {
+                    stack.push(next);
+                    marked[next] = true;
+                    announce();
+                    dashEdgeTo[next] = s;
+                    WQU.union(next, s);
+                } else if (next != dashEdgeTo[s] && WQU.connected(next, s)) {
+                    cycle = true;
+                    int last = dashEdgeTo[next];
+                    edgeTo[last] = next;
+                    edgeTo[next] = s;
+                    int backTrace = s;
+                    while (backTrace != last) {
+                        edgeTo[backTrace] = dashEdgeTo[backTrace];
+                        announce();
+                        backTrace = dashEdgeTo[backTrace];
+                    }
+                    announce();
+                    break;
+                }
+            }
+        }
+    }
 }
 
